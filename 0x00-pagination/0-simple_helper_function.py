c19944fa1d7ff3242e2a49b98f1515b96d2556fe
Cynthia Dike <Maa-mmy>
2023-08-20 22:55:12 +0100

    Simple helper function

--- 0x00-pagination/0-simple_helper_function.py
+++ 0x00-pagination/0-simple_helper_function.py
@@ -1,7 +1,8 @@
 #!/usr/bin/env python3
 """ takes two integer arguments and returns a tuple of size two """
+from typing import Tuple
 
-def index_range(page: int, page_size: int):
+def index_range(page: int, page_size: int) -> Tuple[int, int]:
     """
     Range of the page
 
@@ -12,11 +13,8 @@ def index_range(page: int, page_size: int):
     Return:
     tuple with the start and end
 
-     """
-    if page < 1 or page_size < 1:
-        return None
-    
-    start_index = (page - 1) * page_size
-    end_index = (page * page_size)
+    """
+    start_index: int = (page - 1) * page_size
+    end_index: int = (page * page_size)
 
     return (start_index, end_index)
