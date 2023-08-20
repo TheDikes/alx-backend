4eb1c2d6a415e9abacc45003259ae314db2aee60
Cynthia Dike <Maa-mmy>
2023-08-20 22:38:46 +0100

    Simple helper function

--- 0x00-pagination/0-simple_helper_function.py
+++ 0x00-pagination/0-simple_helper_function.py
@@ -3,10 +3,21 @@
 from typing import Tuple
 
 def index_range(page: int, page_size: int) -> Tuple[int, int]:
+    """
+    Range of the page
+
+    Args: 
+    page: current page
+    page_size: Total size of the page
+
+    Return:
+    tuple with the start and end
+         
+     """
     if page < 1 or page_size < 1:
         return None
     
-    start_index: int = (page - 1) * page_size
-    end_index: int= (page * page_size)
+    start_index = (page - 1) * page_size
+    end_index = (page * page_size)
 
     return (start_index, end_index)
