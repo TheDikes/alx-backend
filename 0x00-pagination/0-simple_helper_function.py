cd14e393614ae51351ae714d8b542126711d6831
Cynthia  Dike <113703996+TheDikes@users.noreply.github.com>
2023-08-20 22:23:44 +0100

    0-simple_helper_function.py

--- 0x00-pagination/0-simple_helper_function.py
+++ 0x00-pagination/0-simple_helper_function.py
@@ -1,6 +1,5 @@
 #!/usr/bin/env python3
-""" A function that takes two integer arguments `page` and `page_size`
-and returns a tuple of size two """
+""" takes two integer arguments and returns a tuple of size two """
 from typing import Tuple
 
 def index_range(page: int, page_size: int):
