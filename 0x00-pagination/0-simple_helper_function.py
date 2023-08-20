5e768b0724619d18a2b894ea0f9077baf0b61132
Cynthia  Dike <113703996+TheDikes@users.noreply.github.com>
2023-08-20 22:25:14 +0100

    0-simple_helper_function.py

--- 0x00-pagination/0-simple_helper_function.py
+++ 0x00-pagination/0-simple_helper_function.py
@@ -2,7 +2,7 @@
 """ takes two integer arguments and returns a tuple of size two """
 from typing import Tuple
 
-def index_range(page: int, page_size: int):
+def index_range(page: int, page_size: int) -> Tuple[int, int]:
     if page < 1 or page_size < 1:
         return None
     
